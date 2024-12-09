71f88bc0fd0c3fa3e405489f993ef15b76166bfb
karthikLVS <157057444+karthikLVS@users.noreply.github.com>
2024-12-09 18:31:33 +0530

    firstcommit

---
+++ src/main/java/com/klef/jfsd/exam/demo/JfsdlabexamApplication.java
@@ -0,0 +1,11 @@
+package com.klef.jfsd.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsdlabexamApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JfsdlabexamApplication.class, args);
+	}
+}
